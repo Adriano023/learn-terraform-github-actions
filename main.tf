c0272b5de0955f309664a7061a9e567260bfc864
Adriano <la_tua_email_di_github@example.com>
2026-08-24 19:46:33 +0200

    Fix AWS region and Terraform cloud block

--- main.tf
+++ main.tf
@@ -15,16 +15,16 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "test_02332"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "learn-terraform-github-actions"
     }
   }
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "us-west-1"
 }
 
 resource "random_pet" "sg" {}
