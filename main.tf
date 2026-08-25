b37c68da5a83cd062773344b8de961a250af6791
Adriano <la_tua_email_di_github@example.com>
2026-08-25 15:29:53 +0200

    Deploy EC2 instance with docker microservice

--- main.tf
+++ main.tf
@@ -3,6 +3,10 @@
 
 terraform {
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
+    }
     random = {
       source  = "hashicorp/random"
       version = "3.4.3"
@@ -12,18 +16,66 @@ terraform {
 
   cloud {
     organization = "test_02332"
-
     workspaces {
       name = "learn-terraform-github-actions"
     }
   }
 }
 
-# Questa risorsa non tocca AWS, genera solo un nome casuale!
-resource "random_pet" "server_name" {
-  length = 2
+provider "aws" {
+  region = "us-east-1"
 }
 
-output "success_message" {
-  value = "Pipeline CI/CD completata al 100%! Risorsa simulata creata: ${random_pet.server_name.id}"
+resource "random_pet" "sg" {}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+
+  # ECCO LA MODIFICA PER IL PROF: Installiamo Docker e avviamo un container!
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y docker.io
+              systemctl start docker
+              systemctl enable docker
+              docker run -d -p 8080:80 --name my-microservice nginx
+              EOF
+}
+
+resource "aws_security_group" "web-sg" {
+  name = "${random_pet.sg.id}-sg"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "web-address" {
+  value = "${aws_instance.web.public_dns}:8080"
 }
